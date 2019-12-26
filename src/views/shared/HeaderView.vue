7820975ae306fb4babee66fd304abb4ba0180bf9
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-26 15:11:58 -0500

    Create HeaderView.vue

---
+++ src/views/shared/HeaderView.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <h1> Routing Application using Vue </h1>
+    <router-link to="/info"><button> Search Server </button></router-link>
+    <router-link to="/history"><button> History </button></router-link>
+  </v-container>
+</template>
+
+
+<script>
+
+export default {
+  name: 'HeaderView',
+  components: {}
+}
+
+</script>

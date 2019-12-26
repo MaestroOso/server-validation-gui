a3bfe9d8a2d0b06476c60300415648786f66c945
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-26 15:46:00 -0500

    Create ConsultPageView.vue

---
+++ src/views/ConsultPageView.vue
@@ -0,0 +1,20 @@
+<template>
+  <span>
+    <header-view></header-view>
+    <server-info></server-info>
+  </span>
+</template>
+
+
+<script>
+import ServerInfo from '@/components/ServerInfo'
+import HeaderView from './shared/HeaderView'
+
+export default {
+  name: 'ConsultPageView',
+  components: {
+    ServerInfo,
+    HeaderView
+  }
+}
+</script>

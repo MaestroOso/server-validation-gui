8c191accf95b4b8be16f11bfb11c4bd8dce6b005
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-26 15:46:19 -0500

    Create HistoryPage.vue

---
+++ src/views/HistoryPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <span>
+    <header-view></header-view>
+    <history-list></history-list>
+  </span>
+</template>
+
+
+<script>
+import HistoryList from '@/components/HistoryList'
+import HeaderView from './shared/HeaderView'
+
+export default {
+  name: 'HistoryPageView',
+  components: {
+    HistoryList,
+    HeaderView
+  }
+}
+</script>

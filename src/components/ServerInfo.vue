3ff99a1398c655668b9f02be2cda1f48e6fa5003
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-26 23:05:44 -0500

    Update ServerInfo.vue

--- src/components/ServerInfo.vue
+++ src/components/ServerInfo.vue
@@ -2,12 +2,33 @@
     <v-container>
         <v-layout justify-center align-center column>
             <h4> Lookup for information </h4><br>
-            Insert domain (e.g. truora.com): <input v-model="domain" placeholder="Domain"><br>
+            Insert domain (e.g. truora.com): <input v-model="domain" placeholder="Domain">
             <button v-on:click="searchdomain">Search</button>
         </v-layout>
         <br>
+
         <div v-if="serverfound">
-          {{ serverdata }}
+          {{ servermsg }}
+          Results for {{ this.domain }}: <br>
+          Server changed: {{ serverdata.ServersChanged }} <br>
+          Ssl grade: {{ serverdata.SslGrade }} <br>
+          Previous Ssl Grade: {{ serverdata.PreviousSslGrade }} <br>
+          Logo: {{ serverdata.Logo }} <br>
+          Is Down: {{ serverdata.Is_down }} <br>
+          Servers:
+          <div v-if="serverdata.Servers != null">
+            <ul id="history-list">
+              <li v-for="server in serverdata.Servers" v-bind:key="server.Address">
+                Address: {{ server.Address }} <br>
+                Ssl Grade: {{ server.SslGrade }} <br>
+                Country: {{ server.Country }} <br>
+                Owner: {{ server.Owner }} <br>
+              </li>
+            </ul>
+          </div>
+          <div v-else>
+              No servers available
+          </div>
         </div>
     </v-container>
 </template>
@@ -22,11 +43,15 @@ export default {
         alert("Domain can't be empty")
         return;
       }
-      this.serverfound = true;
-      this.serverdata = 'Loading...'
+      this.serverfound = false;
+      this.servermsg = 'Loading...'
       this.$http.get('http://localhost:8888/serverInfo/' + this.domain).then( response => {
         if(response.status == "200") {
-          this.serverdata = response.body;
+          this.serverfound = true;
+          this.serverdata = JSON.parse( JSON.stringify( response.body ) )
+          this.servermsg = ''
+        } else {
+          alert("Error on server lookup")
         }
       });
     },
@@ -35,6 +60,7 @@ export default {
     return {
       serverfound: false,
       serverdata: 'Loading...',
+      servermsg: 'Loading...'
     }
   }
 };
